d9e3ba092b86c3906fa370e8febcfcb511017814
pvl-mr <mariyapavlova2001@mail.ru>
2022-05-23 22:55:52 +0400

    add game

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody rb;
+    [SerializeField]
+    float power;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+            rb.AddForce(Vector3.forward * power);
+    }
+}
